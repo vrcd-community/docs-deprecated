4f71f96855c66f9d2e53fed6c72a83bd067588e3
Misaka-L <lipww1234@foxmail.com>
2023-08-31 04:28:47 +0800

    feature: feedback

--- docs/.vitepress/theme/components/FeedBack.vue
+++ docs/.vitepress/theme/components/FeedBack.vue
@@ -1,13 +1,38 @@
 <script lang="ts" setup>
 import { VPButton } from 'vitepress/theme'
-import { ref } from 'vue'
+import { ref, type Ref } from 'vue'
 
 const openFeedbackModal = ref(false)
 const url = ref('')
+const content = ref('')
+const form: Ref<null | HTMLFormElement> = ref(null)
 
 function showModal() {
   openFeedbackModal.value = true
   url.value = location.href
+}
+
+function submit(event: Event) {
+  event.preventDefault()
+  openFeedbackModal.value = false
+
+  if (form.value === null) return;
+  const formData = new FormData(form.value);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    // @ts-ignore
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(response => {
+      if (response.ok) {
+        content.value = ''
+        return
+      }
+
+      alert(`提交反馈失败: ${response.statusText}`)
+    })
 }
 </script>
 
@@ -17,10 +42,11 @@ function showModal() {
     <div v-if="openFeedbackModal" class="modal">
       <div class="model-content">
         <h1>为该页提供建议</h1>
-        <form name="feedback" method="POST" data-netlify="true">
-          <p><label>页面 URL<input v-model="url" type="url" name="url" disabled /></label>
-          </p>
-          <p><label>反馈内容<textarea name="content" placeholder="反馈内容"></textarea></label></p>
+        <form ref="form" @submit="submit">
+          <input type="hidden" name="form-name" value="feedback" />
+          <input v-model="url" type="hidden" required name="url" />
+          <p>页面 URL: {{ url }}</p>
+          <p><label>反馈内容<textarea v-model="content" required name="content" placeholder="反馈内容"></textarea></label></p>
           <div style="display: flex; justify-content: right;">
             <VPButton @click="openFeedbackModal = false" text="取消并关闭" theme="alt" />
             <VPButton type="submit" text="提交" style="margin-left: 1rem;" />
@@ -46,14 +72,6 @@ function showModal() {
   font-size: x-large;
   font-weight: bold;
   margin-bottom: 1rem;
-}
-
-.model-content input {
-  background-color: var(--vp-c-bg-soft);
-  margin: 4px;
-  padding: 4px 12px;
-  border-radius: 8px;
-  width: 100%;
 }
 
 .model-content textarea {
